96bfb7c0104e6f7981580d64f7474d5278885a6f
scotts <juveduke@gmail.com>
2019-12-15 19:45:59 -0500

    Fix times since they are the worst

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,6 @@ import * as cookies from '../etc/cookies.json'
 import * as serviceAccount from '../etc/serviceAccountKey.json'
 
 const request = require('request')
-//const firebase = require('firebase')
 const admin = require('firebase-admin');
 
 admin.initializeApp({
@@ -22,77 +21,77 @@ const options = {
 }
 const re = /"scoreList":(\[\{.*\}\])\}/
 
-function fetchScores() {
-  request.get(options, function(error: any, response: Response, body: String) {
-    const match: RegExpMatchArray | null = body.match(re)
-    if (match !== null && match.length > 1) {
-      const results: any = JSON.parse(match[1])
-      const date: Date = new Date()
-      if (
-        date.getDay() === 6 && date.getHours() >= 18 ||
-        date.getHours() >= 22
-      ) {
-        date.setTime(date.getTime() + (6 * 60 * 60 * 1000))
-      }
-      const dayOfWeek: number = date.getDay()
-      const dateStr: string = date.toISOString().split('T')[0]
-      console.log('dateStr: ' + dateStr)
-      const timestamp: number = Date.now()
-      results.forEach((result: any) => {
-        if (result.finished) {
-          const split: Array<string> = result.solveTime.split(':')
-          let minutes: number = 0
-          let seconds: number = 0
-          let hours: number = 0
-          if (split.length === 1) {
-            seconds = parseInt(split[0])
-          } else if (split.length === 2) {
-            minutes = parseInt(split[0])
-            seconds = parseInt(split[1])
-          } else if (split.length === 3) {
-            hours = parseInt(split[0])
-            minutes = parseInt(split[1])
-            seconds = parseInt(split[2])
-          }
-          const entry = {
-            name: result.name,
-            rank: parseInt(result.rank),
-            solveTime: (hours * 3600) + (minutes * 60) + seconds,
-            dayOfWeek: dayOfWeek,
-            timestamp: timestamp,
-            date: dateStr
-          }
-          console.log("Processing entry:")
-          console.log(entry)
+function upsertEntry(entry: any) {
+  console.log("Upsert entry:")
+  console.log(entry)
 
-          const userEntries = entriesRef
-            .where("name", "==", result.name)
-            .where("date", "==", dateStr)
-          userEntries.get().then(function (querySnapshot: any) {
-            if (querySnapshot.empty) {
-              console.log("[" + dateStr + "]" + "Adding entry for: " + result.name)
-              entriesRef.add(entry)
-            } else {
-              console.log("[" + dateStr + "]" + "Updating entry for: " + result.name)
-              querySnapshot.forEach(function (queryDocSnapshot: any) {
-                queryDocSnapshot.ref.set(entry)
-              })
-            }
-          })
-        }
+  const userEntries = entriesRef
+    .where("name", "==", entry.name)
+    .where("date", "==", entry.date)
+  userEntries.get().then(function (querySnapshot: any) {
+    if (querySnapshot.empty) {
+      console.log("[" + entry.dateStr + "] Adding entry for: " + entry.name)
+        //entriesRef.add(entry)
+    } else {
+      console.log("[" + entry.dateStr + "] Updating entry for: " + entry.name)
+      querySnapshot.forEach(function (queryDocSnapshot: any) {
+          //queryDocSnapshot.ref.set(entry)
       })
     }
   })
 }
 
-exports.weekdayRun = functions.pubsub.schedule('56 21 * * 0-4')
+function parseResults(results: Array<any>) {
+  const localDt: string = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
+  const date: Date = new Date(localDt)
+  if (
+    ((date.getDay() === 6 || date.getDay() === 0) && date.getHours() >= 18) ||
+    date.getHours() >= 22
+  ) {
+    // roll forward to the next day since they release the evening before
+    date.setTime(date.getTime() + (6 * 60 * 60 * 1000))
+  }
+  const dayOfWeek: number = date.getDay()
+  const timestamp: number = Date.now()
+  const dateStr: string = date.toISOString().split('T')[0]
+  console.log('Processing results for date: ' + dateStr)
+  results.forEach((result: any) => {
+    if (result.finished) {
+      const split: Array<string> = result.solveTime.split(':')
+      let seconds: number = 0
+      for (let i: number = 0; i < split.length; i++) {
+        seconds += Math.pow(60, split.length - i - 1) * parseInt(split[i])
+      }
+      upsertEntry({
+        name: result.name,
+        rank: parseInt(result.rank),
+        solveTime: seconds,
+        dayOfWeek: dayOfWeek,
+        timestamp: timestamp,
+        date: dateStr
+      })
+    }
+  })
+}
+
+function fetchScores() {
+  request.get(options, function(error: any, response: Response, body: String) {
+    const match: RegExpMatchArray | null = body.match(re)
+    if (match !== null && match.length > 1) {
+      const results: any = JSON.parse(match[1])
+      parseResults(results)
+    }
+  })
+}
+
+exports.weekdayRun = functions.pubsub.schedule('56 21 * * 1-5')
   .timeZone('America/New_York')
   .onRun((context) => {
     fetchScores()
     return null
   })
 
-exports.weekendRun = functions.pubsub.schedule('56 17 * * 5,6')
+exports.weekendRun = functions.pubsub.schedule('56 17 * * 0,6')
   .timeZone('America/New_York')
   .onRun((context) => {
     fetchScores()
